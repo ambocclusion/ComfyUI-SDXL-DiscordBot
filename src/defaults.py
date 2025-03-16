06fb26319ebd81ee0342a8d68dcdca45213deb4a
occlusion <bslashingu@gmail.com>
2025-03-16 11:19:27 -0400

    refactored default configs to be simpler

--- src/defaults.py
+++ src/defaults.py
@@ -5,264 +5,70 @@ from src.image_gen.ImageWorkflow import *
 config = configparser.ConfigParser()
 config.read("config.properties")
 
-def get_default_from_config(section : str, option : str) -> str:
+def get_default_from_config(section : str, option : str, default = None) -> str:
     if section not in config:
         print(f"Section {section} not found in config")
-        return None
+        return default
     if option not in config[section]:
-        return None
+        return default
     return config[section][option]
 
-SD15_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.SD15, # model_type
-    None, # workflow_type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("SD15_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths TODO add lora and lora strength defaults
-    get_default_from_config("SD15_GENERATION_DEFAULTS", "ASPECT_RATIO"),
-    get_default_from_config("SD15_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("SD15_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("SD15_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("SD15_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("SD15_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "imagine",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("SD15_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "CLIP_SKIP")),  # clip_skip
-    scheduler=get_default_from_config("SD15_GENERATION_DEFAULTS", "SCHEDULER"),
-    llm_profile=get_default_from_config("SD15_GENERATION_DEFAULTS", "LLM_PROFILE"),
-    use_tensorrt=bool(get_default_from_config("SD15_GENERATION_DEFAULTS", "USE_TENSORRT")) or False,
-    tensorrt_model=get_default_from_config("SD15_GENERATION_DEFAULTS", "TENSORRT_MODEL"),
-)
+def get_defaults_for_command(section: str, model_type: ModelType, slash_command: str) -> ImageWorkflow:
+    workflow = ImageWorkflow(
+        model_type,
+        None, # workflow_type
+        None, # prompt
+        None, # negative_prompt
+        get_default_from_config(section, "MODEL"),
+        None, # loras
+        None, # lora_strengths TODO add lora and lora strength defaults
+        get_default_from_config(section, "ASPECT_RATIO"),
+        get_default_from_config(section, "SAMPLER"),
+        int(get_default_from_config(section, "NUM_STEPS", 20)),
+        float(get_default_from_config(section, "CFG_SCALE", 4.0)),
+        float(get_default_from_config(section, "DENOISE_STRENGTH", 0.8)),
+        int(get_default_from_config(section, "BATCH_SIZE", 1)), # batch_size
+        None, # seed
+        None, # filename
+        slash_command,
+        None, # inpainting_prompt
+        int(get_default_from_config(section, "INPAINTING_DETECTION_THRESHOLD", 200)),
+        int(get_default_from_config(section, "CLIP_SKIP", -1)),
+        None, # filename2
+        bool(get_default_from_config(section, "ACCELERATOR_ENABLED", False)),
+        get_default_from_config(section, "ACCELERATOR_LORA_NAME"),
+        get_default_from_config(section, "SCHEDULER"),
+        float(get_default_from_config(section, "MIN_CFG", 0.0)),
+        float(get_default_from_config(section, "MOTION", 0.0)),
+        float(get_default_from_config(section, "AUGMENTATION", 0.0)),
+        int(get_default_from_config(section, "FPS", 0)),
+        get_default_from_config(section, "DEFAULT_STYLE_PROMPT"),
+        get_default_from_config(section, "DEFAULT_NEGATIVE_STYLE_PROMPT"),
+        get_default_from_config(section, "VAE"),
+        get_default_from_config(section, "DETAILING_CONTROLNET"),
+        get_default_from_config(section, "DETAILING_CONTROLNET_STRENGTH"),
+        get_default_from_config(section, "DETAILING_CONTROLNET_END_PERCENT"),
+        bool(get_default_from_config(section, "USE_LLM_BY_DEFAULT", False)),
+        get_default_from_config(section, "LLM_PROFILE"),
+        bool(get_default_from_config(section, "USE_ALIGN_YOUR_STEPS", False)),
+        bool(get_default_from_config(section, "USE_TENSORRT", False)),
+        get_default_from_config(section, "TENSORRT_MODEL"),
+        float(get_default_from_config(section, "MASHUP_IMAGE1_STRENGTH", 1.0)),
+        float(get_default_from_config(section, "MASHUP_IMAGE2_STRENGTH", 1.0)) ,
+    )
+    return workflow
 
-SDXL_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.SDXL,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "ASPECT_RATIO"),
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("SDXL_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("SDXL_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "sdxl",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "CLIP_SKIP")),  # clip_skip
-    None,   # filename2
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "ACCELERATOR_ENABLED"),
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "ACCELERATOR_LORA_NAME"),
-    get_default_from_config("SDXL_GENERATION_DEFAULTS", "SCHEDULER"),
-    style_prompt=get_default_from_config("SDXL_GENERATION_DEFAULTS", "DEFAULT_STYLE_PROMPT"),
-    negative_style_prompt=get_default_from_config("SDXL_GENERATION_DEFAULTS", "DEFAULT_NEGATIVE_STYLE_PROMPT"),
-    detailing_controlnet=get_default_from_config("SDXL_GENERATION_DEFAULTS", "DETAILING_CONTROLNET"),
-    llm_profile=get_default_from_config("SDXL_GENERATION_DEFAULTS", "LLM_PROFILE"),
-    use_align_your_steps=get_default_from_config("SDXL_GENERATION_DEFAULTS", "USE_ALIGN_YOUR_STEPS"),
-    use_tensorrt=bool(get_default_from_config("SDXL_GENERATION_DEFAULTS", "USE_TENSORRT")),
-    tensorrt_model=get_default_from_config("SDXL_GENERATION_DEFAULTS", "TENSORRT_MODEL"),
-)
-
-CASCADE_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.CASCADE,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("CASCADE_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    get_default_from_config("CASCADE_GENERATION_DEFAULTS", "ASPECT_RATIO"),  # aspect_ratio
-    get_default_from_config("CASCADE_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("CASCADE_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("CASCADE_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("CASCADE_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("CASCADE_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "cascade",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("SDXL_GENERATION_DEFAULTS", "CLIP_SKIP")),  # clip_skip
-    llm_profile=get_default_from_config("CASCADE_GENERATION_DEFAULTS", "LLM_PROFILE"),
-)
-
-SVD_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.VIDEO,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("VIDEO_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    None,  # aspect_ratio
-    get_default_from_config("VIDEO_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "CFG_SCALE")),
-    int(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # denoise_strength
-    None,  # seed
-    None,  # filename
-    "svd",  # slash_command
-    min_cfg=float(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "MIN_CFG")),
-    motion=int(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "MOTION")),
-    augmentation=float(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "AUGMENTATION")),
-    fps=int(get_default_from_config("VIDEO_GENERATION_DEFAULTS", "FPS")),
-)
-
-WAN_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.VIDEO,  # model_type
-    WorkflowType.wan,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("WAN_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    None,  # aspect_ratio
-    get_default_from_config("WAN_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("WAN_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("WAN_GENERATION_DEFAULTS", "CFG_SCALE")),
-    int(get_default_from_config("WAN_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # denoise_strength
-    None,  # seed
-    None,  # filename
-    "video",  # slash_command
-    fps=int(get_default_from_config("WAN_GENERATION_DEFAULTS", "FPS")),
-    style_prompt=get_default_from_config("WAN_GENERATION_DEFAULTS", "DEFAULT_STYLE_PROMPT"),
-    negative_style_prompt=get_default_from_config("WAN_GENERATION_DEFAULTS", "DEFAULT_NEGATIVE_STYLE_PROMPT"),
-)
-
-IMAGE_WAN_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.VIDEO,  # model_type
-    WorkflowType.image_wan,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    None,  # aspect_ratio
-    get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "CFG_SCALE")),
-    int(get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # denoise_strength
-    None,  # seed
-    None,  # filename
-    "video",  # slash_command
-    fps=int(get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "FPS")),
-    style_prompt=get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "DEFAULT_STYLE_PROMPT"),
-    negative_style_prompt=get_default_from_config("IMAGE_WAN_GENERATION_DEFAULTS", "DEFAULT_NEGATIVE_STYLE_PROMPT"),
-)
-
-
-PONY_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.SDXL,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("PONY_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    get_default_from_config("PONY_GENERATION_DEFAULTS", "ASPECT_RATIO"),  # aspect_ratio
-    get_default_from_config("PONY_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("PONY_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("PONY_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("PONY_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("PONY_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "pony",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("PONY_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("PONY_GENERATION_DEFAULTS", "CLIP_SKIP")),
-    style_prompt=get_default_from_config("PONY_GENERATION_DEFAULTS", "DEFAULT_STYLE_PROMPT"),
-    negative_style_prompt=get_default_from_config("PONY_GENERATION_DEFAULTS", "DEFAULT_NEGATIVE_STYLE_PROMPT"),
-    vae=get_default_from_config("PONY_GENERATION_DEFAULTS", "VAE"),
-    detailing_controlnet=get_default_from_config("PONY_GENERATION_DEFAULTS", "DETAILING_CONTROLNET"),
-    llm_profile=get_default_from_config("PONY_GENERATION_DEFAULTS", "LLM_PROFILE"),
-    use_align_your_steps=get_default_from_config("PONY_GENERATION_DEFAULTS", "USE_ALIGN_YOUR_STEPS"),
-)
-
-SD3_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.SD3,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("SD3_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    get_default_from_config("SD3_GENERATION_DEFAULTS", "ASPECT_RATIO"),  # aspect_ratio
-    get_default_from_config("SD3_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("SD3_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("SD3_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("SD3_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("SD3_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "sd3",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("SD3_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("SD3_GENERATION_DEFAULTS", "CLIP_SKIP")),
-    llm_profile=get_default_from_config("SD3_GENERATION_DEFAULTS", "LLM_PROFILE"),
-    use_align_your_steps=get_default_from_config("SD3_GENERATION_DEFAULTS", "USE_ALIGN_YOUR_STEPS"),
-    scheduler=get_default_from_config("SD3_GENERATION_DEFAULTS", "SCHEDULER"),
-    use_tensorrt=bool(get_default_from_config("SD3_GENERATION_DEFAULTS", "USE_TENSORRT")) or False,
-    tensorrt_model=get_default_from_config("SD3_GENERATION_DEFAULTS", "TENSORRT_MODEL"),
-)
-
-FLUX_GENERATION_DEFAULTS = ImageWorkflow(
-    ModelType.FLUX,  # model_type
-    None,  # workflow type
-    None,  # prompt
-    None,  # negative_prompt
-    get_default_from_config("FLUX_GENERATION_DEFAULTS", "MODEL"),
-    None,  # loras
-    None,  # lora_strengths
-    get_default_from_config("FLUX_GENERATION_DEFAULTS", "ASPECT_RATIO"),  # aspect_ratio
-    get_default_from_config("FLUX_GENERATION_DEFAULTS", "SAMPLER"),
-    int(get_default_from_config("FLUX_GENERATION_DEFAULTS", "NUM_STEPS")),
-    float(get_default_from_config("FLUX_GENERATION_DEFAULTS", "CFG_SCALE")),
-    float(get_default_from_config("FLUX_GENERATION_DEFAULTS", "DENOISE_STRENGTH")),
-    int(get_default_from_config("FLUX_GENERATION_DEFAULTS", "BATCH_SIZE")),  # batch_size
-    None,  # seed
-    None,  # filename
-    "flux",  # slash_command
-    None,  # inpainting_prompt
-    int(get_default_from_config("FLUX_GENERATION_DEFAULTS", "INPAINTING_DETECTION_THRESHOLD")),  # inpainting_detection_threshold
-    int(get_default_from_config("FLUX_GENERATION_DEFAULTS", "CLIP_SKIP")),
-    llm_profile=get_default_from_config("FLUX_GENERATION_DEFAULTS", "LLM_PROFILE"),
-    use_align_your_steps=get_default_from_config("FLUX_GENERATION_DEFAULTS", "USE_ALIGN_YOUR_STEPS"),
-    scheduler=get_default_from_config("FLUX_GENERATION_DEFAULTS", "SCHEDULER"),
-    use_tensorrt=bool(get_default_from_config("FLUX_GENERATION_DEFAULTS", "USE_TENSORRT")) or False,
-    tensorrt_model=get_default_from_config("FLUX_GENERATION_DEFAULTS", "TENSORRT_MODEL"),
-    mashup_image_strength=float(get_default_from_config("FLUX_GENERATION_DEFAULTS", "MASHUP_IMAGE1_STRENGTH")),
-    mashup_inputimage_strength=float(get_default_from_config("FLUX_GENERATION_DEFAULTS", "MASHUP_IMAGE2_STRENGTH")),
-)
-
-ADD_DETAIL_DEFAULTS = ImageWorkflow(
-    None,
-    WorkflowType.add_detail,
-    None,
-    denoise_strength=float(get_default_from_config("ADD_DETAIL_DEFAULTS", "DENOISE_STRENGTH")),
-    batch_size=int(get_default_from_config("ADD_DETAIL_DEFAULTS", "BATCH_SIZE")),
-    detailing_controlnet_strength=float(get_default_from_config("ADD_DETAIL_DEFAULTS", "DETAILING_CONTROLNET_STRENGTH")),
-    detailing_controlnet_end_percent=float(get_default_from_config("ADD_DETAIL_DEFAULTS", "DETAILING_CONTROLNET_END_PERCENT")),
-)
-
-UPSCALE_DEFAULTS = ImageWorkflow(
-    None,
-    WorkflowType.upscale,
-    None,
-    model=get_default_from_config("UPSCALE_DEFAULTS", "MODEL"),
-)
+SD15_GENERATION_DEFAULTS = get_defaults_for_command("SD15_GENERATION_DEFAULTS", ModelType.SD15, "imagine")
+SDXL_GENERATION_DEFAULTS = get_defaults_for_command("SDXL_GENERATION_DEFAULTS", ModelType.SDXL, "sdxl")
+CASCADE_GENERATION_DEFAULTS = get_defaults_for_command("CASCADE_GENERATION_DEFAULTS", ModelType.CASCADE, "cascade")
+SVD_GENERATION_DEFAULTS = get_defaults_for_command("SVD_GENERATION_DEFAULTS", ModelType.VIDEO, "video")
+WAN_GENERATION_DEFAULTS = get_defaults_for_command("WAN_GENERATION_DEFAULTS", ModelType.SD15, "wan")
+IMAGE_WAN_GENERATION_DEFAULTS = get_defaults_for_command("IMAGE_WAN_GENERATION_DEFAULTS", ModelType.SD15, "image_wan")
+PONY_GENERATION_DEFAULTS = get_defaults_for_command("PONY_GENERATION_DEFAULTS", ModelType.PONY, "pony")
+SD3_GENERATION_DEFAULTS = get_defaults_for_command("SD3_GENERATION_DEFAULTS", ModelType.SD3, "sd3")
+FLUX_GENERATION_DEFAULTS = get_defaults_for_command("FLUX_GENERATION_DEFAULTS", ModelType.FLUX, "flux")
+ADD_DETAIL_DEFAULTS = get_defaults_for_command("ADD_DETAIL_DEFAULTS", ModelType.SDXL, "add_detail")
+UPSCALE_DEFAULTS = get_defaults_for_command("UPSCALE_DEFAULTS", ModelType.SDXL, "upscale")
 
 COMMAND_DEFAULTS = {
     "imagine": SDXL_GENERATION_DEFAULTS,
